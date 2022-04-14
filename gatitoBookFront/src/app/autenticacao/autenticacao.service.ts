bca008502d3b05ee9aae7290c82918f3a0793daf
Emerson916 <emersons.a296@gmail.com>
2022-04-14 16:09:36 -0300

    Criando o service de autenticação, registro do usuario

---
+++ gatitoBookFront/src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class AutenticacaoService {
+  private url = 'http://localhost:3000/user/login';
+
+  constructor(private httpClient: HttpClient) {}
+
+  autenticar(usuario: string, senha: string): Observable<any> {
+    return this.httpClient.post(this.url, {
+      userName: usuario,
+      password: senha,
+    });
+  }
+}

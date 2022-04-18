dc09dfadb86039fb6cf9850a4e735b42381094ae
Emerson916 <emersons.a296@gmail.com>
2022-04-18 16:53:23 -0300

    Adicionando novo modulo de animais

---
+++ gatitoBookFront/src/app/animals/animals-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListAnimalsComponent } from './list-animals/list-animals.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListAnimalsComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AnimalsRoutingModule {}

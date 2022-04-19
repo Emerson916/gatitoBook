a58dd4b116b4fbe30f620d915e9664a4f0f9c15f
Emerson916 <emersons.a296@gmail.com>
2022-04-19 12:55:34 -0300

    Pegando os dados dos input e criando um novo usuario

--- gatitoBookFront/src/app/home/home.module.ts
+++ gatitoBookFront/src/app/home/home.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
-import { MensagemModule } from '../components/mensagem/mensagem.module';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MensagemModule } from '../components/mensagem/mensagem.module';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent, RegisterComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FormsModule,
+    MensagemModule,
+    ReactiveFormsModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- gatitoBookFront/src/app/home/register/register.component.ts
+++ gatitoBookFront/src/app/home/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Register } from './register';
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  newUserForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private registerService: RegisterService
+  ) {}
 
   ngOnInit(): void {
+    this.newUserForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  cadastrar() {
+    const newUser = this.newUserForm.getRawValue() as Register;
+    console.log(newUser);
+  }
 }

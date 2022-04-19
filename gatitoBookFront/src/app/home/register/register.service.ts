2e558dadac8ab519684a9163b9c3cae7ce00eb18
Emerson916 <emersons.a296@gmail.com>
2022-04-19 11:46:13 -0300

    Consumindo endpont do backend para registrar um novo usuario

---
+++ gatitoBookFront/src/app/home/register/register.service.ts
@@ -0,0 +1,14 @@
+import { Register } from './register';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterService {
+  constructor(private http: HttpClient) {}
+
+  registerNewUser(newUser: Register) {
+    return this.http.post('http://localhost:3000/user/signup', newUser);
+  }
+}

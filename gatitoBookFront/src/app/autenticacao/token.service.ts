dbbc39bd2d1f51886cbf125a84ebbe74dcae4fc2
Emerson916 <emersons.a296@gmail.com>
2022-04-20 12:44:17 -0300

    Criando funções de validação do token jwt

---
+++ gatitoBookFront/src/app/autenticacao/token.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  constructor() {}
+
+  returnToken() {
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem(KEY, token);
+  }
+
+  deleteToken() {
+    localStorage.removeItem(KEY);
+  }
+
+  hasToken() {
+    return !!this.returnToken();
+  }
+}

58fbb8cc8cebf940a17e828cd3ac09c57977bd99
Emerson916 <emersons.a296@gmail.com>
2022-04-20 14:17:32 -0300

    Criando o component de rodape

--- gatitoBookFront/src/app/app.module.ts
+++ gatitoBookFront/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HeaderModule } from './components/header/header.module';
+import { RodapeModule } from './components/rodape/rodape.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,7 +8,13 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HeaderModule,
+    RodapeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

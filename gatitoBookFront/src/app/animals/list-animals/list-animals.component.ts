ccd7752cddbcb5e3ead1419019bd183125387ee7
Emerson916 <emersons.a296@gmail.com>
2022-04-25 16:54:33 -0300

    Adicionando perfil do usuario, pegando dados do usuario, nome e email

--- gatitoBookFront/src/app/animals/list-animals/list-animals.component.ts
+++ gatitoBookFront/src/app/animals/list-animals/list-animals.component.ts
@@ -1,15 +1,16 @@
+import { Router } from '@angular/router';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
-  styleUrls: ['./list-animals.component.css']
+  styleUrls: ['./list-animals.component.css'],
 })
 export class ListAnimalsComponent implements OnInit {
+  user$ = this.usuarioService.returnUser();
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
